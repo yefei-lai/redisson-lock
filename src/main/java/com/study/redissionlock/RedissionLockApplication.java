a8bf20f81e62a2201fd1ac6d7600294ad17e0c08
laiyefei <laiyefei0710@163.com>
2021-04-30 15:56:01 +0800

    第十四周 分布式锁1 基于Redisson实现的分布式锁

---
+++ src/main/java/com/study/redissionlock/RedissionLockApplication.java
@@ -0,0 +1,13 @@
+package com.study.redissionlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissionLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissionLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/redissionlock/RedissionLockApplicationTests.java
@@ -0,0 +1,40 @@
+package com.study.redissionlock;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+@Slf4j
+class RedissionLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testRedissonLock(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:7181");
+        RedissonClient redisson = Redisson.create(config);
+
+        RLock rLock = redisson.getLock("order");
+        try {
+            rLock.lock(30, TimeUnit.SECONDS);
+            log.info("我获得了锁!");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            log.info("我释放了锁！");
+            rLock.unlock();
+        }
+    }
+
+}
